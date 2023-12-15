8597b2990e9b0bd919e67ea9857d29a0bbb63454
MrShvetsov <shvetsov-tech@yandex.ru>
2023-12-16 01:09:44 +0700

    1234e

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <title>Регистрация</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/master.css">
     <title>KRS</title>
     
 </head>
@@ -14,6 +15,38 @@
         <?php
             include("src/actions/db_connect.php");
         ?>
+
+        <section id="activity">
+            <div class="container">
+                <div class="inner">
+                    <h2 class="heading">Мероприятия</h2>
+                    <div class="cards">
+                        <div class="card">
+                            <div class="left">
+                                <h3 class="heading-card">DevRel Hack 2.0</h3>
+                                <ul class="rules">
+                                    <li class="item">
+                                        <div class="icon"></div>
+                                        Хакатон
+                                    </li>
+                                    <li class="item">
+                                        <div class="icon"></div>
+                                        Команды от 2 до 5 участников
+                                    </li>
+                                    <li class="item">
+                                        <div class="icon"></div>
+                                        200 000 руб и другие призы 
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="right">
+                                <img src="images/1.png" alt="баннер">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         
         <form action="register.php" method="post">
             <label for="name">Имя</label>
--- src/actions/db_connect.php
+++ src/actions/db_connect.php
@@ -1,2 +1,16 @@
 <?php
+  mb_internal_encoding("UTF-8");
+  $host_db = "81.31.246.222";
+  $user_db = "root";
+  $password_db = "3r0to\$z+)9LyZc";
+  $name_db = "devrel";
+
+  $connection_db = new mysqli($host_db, $user_db, $password_db, $name_db);
+  // mysqli_set_charset($connection_db, "utf8");
+  if ($connection_db == False) {
+    echo "Сорян";
+  }
+
+  // $results = musqli_query("SELECT first_name FROM users");
+  // var_dump($results);
 ?>
