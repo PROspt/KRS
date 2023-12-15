8a27ab45f9dfc95a7a72e48ebe4d45a63db9c067
Vyacheslav142 <109301108+Vyacheslav142@users.noreply.github.com>
2023-12-15 23:40:23 +0700

    add style in index

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <title>Регистрация</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>KRS</title>
     
 </head>
