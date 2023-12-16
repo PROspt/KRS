6c44da107ff20a0e3737573998d860af0e0befec
NETEG0N <kostyankozlov2309@gmail.com>
2023-12-17 00:37:37 +0700

    изменение кнопки войти

--- src/blocks/footer.php
+++ src/blocks/footer.php
@@ -18,10 +18,12 @@
       <p>&copy; 2024 РОГАТЫЙ СКОТ. Все права защищены.</p>
 
 
-
+    <div>
       <a href= "admin.php">Переход в админ</a>
       <a href= "index.php">Переход домой</a>
       <a href= "vk_connect.php">Переход вк</a>
+    </div>
+      
 
     </footer>
     </html>
