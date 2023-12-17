e4cb6d54bb2a978b3cb506804a74710f636c8fcc
Vyacheslav142 <109301108+Vyacheslav142@users.noreply.github.com>
2023-12-17 13:35:44 +0700

    add рассылка

--- vk_connect.php
+++ vk_connect.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
   <head>
     <title>Добавление поста в VK</title>
@@ -34,7 +34,7 @@
       });
     </script>
   </body>
-</html>
+</html> -->
 
 
 
@@ -63,57 +63,56 @@ if ($result && isset($result['response'][0])) {
   $groupData = $result['response'][0];
   
   // Вывод полученной информации
-  echo 'ID: ' . $groupData['id'] . '<br>';
-  echo 'Название: ' . $groupData['name'] . '<br>';
+  // echo 'ID: ' . $groupData['id'] . '<br>';
+  // echo 'Название: ' . $groupData['name'] . '<br>';
   
   // Проверка наличия поля "description"
   if (isset($groupData['description'])) {
-    echo 'Описание: ' . $groupData['description'] . '<br>';
+    // echo 'Описание: ' . $groupData['description'] . '<br>';
   } else {
-    echo 'Описание: Нет данных <br>';
+    // echo 'Описание: Нет данных <br>';
   }
   
   // Проверка наличия поля "members_count"
   if (isset($groupData['members_count'])) {
-    echo 'Количество подписчиков: ' . $groupData['members_count'] . '<br>';
+    // echo 'Количество подписчиков: ' . $groupData['members_count'] . '<br>';
   } else {
-    echo 'Количество подписчиков: Нет данных <br>';
+    // echo 'Количество подписчиков: Нет данных <br>';
   }
-  echo "<br>";
+  // echo "<br>";
   // Вывод фото
   if (isset($groupData['photo_200'])) {
-    echo 'Ссылка на фото: <img src="' . $groupData['photo_200'] . '">';
+    // echo '<img src="' . $groupData['photo_200'] . '">';
   }
 } else {
   echo 'Ошибка при получении информации о группе: ' . $response;
 }
-echo "<br>";
 
-  // Получение списка подписчиков группы
-  $membersParams = array(
-    'group_id' => $groupId,
-    'fields' => 'first_name,last_name',
-    'count' => 1000,
-    'access_token' => $accessToken,
-    'v' => '5.131'
-  );
+  // // Получение списка подписчиков группы
+  // $membersParams = array(
+  //   'group_id' => $groupId,
+  //   'fields' => 'first_name,last_name',
+  //   'count' => 1000,
+  //   'access_token' => $accessToken,
+  //   'v' => '5.131'
+  // );
 
-  // Отправка запроса на получение списка подписчиков
-  $membersResponse = file_get_contents('https://api.vk.com/method/groups.getMembers?' . http_build_query($membersParams));
-  $membersResult = json_decode($membersResponse, true);
+  // // Отправка запроса на получение списка подписчиков
+  // $membersResponse = file_get_contents('https://api.vk.com/method/groups.getMembers?' . http_build_query($membersParams));
+  // $membersResult = json_decode($membersResponse, true);
 
-  // Проверка результата
-  if ($membersResult && isset($membersResult['response']['items'])) {
-    $members = $membersResult['response']['items'];
+  // // Проверка результата
+  // if ($membersResult && isset($membersResult['response']['items'])) {
+  //   $members = $membersResult['response']['items'];
 
-    // Вывод списка подписчиков
-    echo 'Список подписчиков:<br>';
-    foreach ($members as $member) {
-      echo 'ID: ' . $member['id'] . ', Имя: ' . $member['first_name'] . ', Фамилия: ' . $member['last_name'] . '<br>';
-    }
-  } else {
-    echo 'Ошибка при получении списка подписчиков: ' . $membersResponse;
-  }
+  //   // Вывод списка подписчиков
+  //   echo 'Список подписчиков:<br>';
+  //   foreach ($members as $member) {
+  //     echo 'ID: ' . $member['id'] . ', Имя: ' . $member['first_name'] . ', Фамилия: ' . $member['last_name'] . '<br>';
+  //   }
+  // } else {
+  //   echo 'Ошибка при получении списка подписчиков: ' . $membersResponse;
+  // }
 
 
 
