178c3c0f013a6ce16b20d4ca1c49766515c4697b
Vyacheslav142 <109301108+Vyacheslav142@users.noreply.github.com>
2023-12-17 16:54:31 +0700

    Update master.js

--- scripts/master.js
+++ scripts/master.js
@@ -1,13 +1,13 @@
-let login = document.querySelector(".header .head-login");
-let modalContainer = document.querySelector("#modal-container");
-let closeModalButton = document.querySelector('#zakroi');
+// let login = document.querySelector(".header .head-login");
+// let modalContainer = document.querySelector("#modal-container");
+// let closeModalButton = document.querySelector('#zakroi');
 
-// Обработчик для открытия модального окна
-login.addEventListener("click", () => {
-  modalContainer.style.display = "block";
-});
+// // Обработчик для открытия модального окна
+// login.addEventListener("click", () => {
+//   modalContainer.style.display = "block";
+// });
 
-// Обработчик для закрытия модального окна
-closeModalButton.addEventListener('click', function () {
-  modalContainer.style.display = "none";
-});
+// // Обработчик для закрытия модального окна
+// closeModalButton.addEventListener('click', function () {
+//   modalContainer.style.display = "none";
+// });
